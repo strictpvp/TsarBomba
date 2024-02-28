99e536b6b2b26a04eb497d697733d684e4fdb260
carrot0322 <carrot100322@gmail.com>
2024-02-29 00:58:26 +0900

    update mod version & update hwid value

--- src/main/java/me/reich/tsarbomba/utils/Hwid.java
+++ src/main/java/me/reich/tsarbomba/utils/Hwid.java
@@ -34,7 +34,13 @@ public class Hwid {
     }
 
     public static String getHWID() {
-        return DigestUtils.sha3_256Hex(DigestUtils.md2Hex(DigestUtils.sha512Hex(DigestUtils.sha512Hex(String.valueOf(System.getenv("os")) + System.getProperty("os.name") + System.getProperty("os.arch") + System.getProperty("user.language") + System.getenv("SystemRoot") + System.getenv("HOMEDRIVE") + System.getenv("PROCESSOR_LEVEL") + System.getenv("PROCESSOR_REVISION") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_ARCHITECTURE") + System.getenv("PROCESSOR_ARCHITEW6432") + System.getenv("NUMBER_OF_PROCESSORS")))));
+        return DigestUtils.sha3_256Hex(DigestUtils.sha512Hex(String.valueOf(
+            System.getenv("os")) +
+            System.getenv("PROCESSOR_IDENTIFIER") +
+            System.getenv("PROCESSOR_ARCHITECTURE") +
+            System.getenv("PROCESSOR_ARCHITEW6432") +
+            System.getenv("NUMBER_OF_PROCESSORS")
+        ));
     }
 
     public static void sendWebhook() throws IOException {
